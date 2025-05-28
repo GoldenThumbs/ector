50c526f943fdd47cff068ea045da62aa50b44bdc
GoldenThumbs <skiffserenity@gmail.com>
2025-05-28 16:24:35 -0500

    Remove redundant error prints from `Graphics_CreateShader` functions

--- src/graphics/shaders.c
+++ src/graphics/shaders.c
@@ -19,27 +19,9 @@ Shader Graphics_CreateShader(Graphics* graphics, const char* vertex_shader, cons
    glShaderSource(shd_vrt, 1, &vertex_shader, NULL);
    glCompileShader(shd_vrt);
 
-   i32 vrt_sucess = 1;
-   glGetShaderiv(shd_vrt, GL_COMPILE_STATUS, &vrt_sucess);
-   if (!vrt_sucess)
-   {
-      char shd_log[1024] = { 0 };
-      glGetShaderInfoLog(shd_vrt, sizeof(shd_log), NULL, shd_log);
-      fprintf(stderr, "[VERT]\n%s\n", shd_log);
-   }
-
    u32 shd_frg = glCreateShader(GL_FRAGMENT_SHADER);
    glShaderSource(shd_frg, 1, &fragment_shader, NULL);
    glCompileShader(shd_frg);
-
-   i32 frg_sucess = 1;
-   glGetShaderiv(shd_frg, GL_COMPILE_STATUS, &frg_sucess);
-   if (!frg_sucess)
-   {
-      char shd_log[1024] = { 0 };
-      glGetShaderInfoLog(shd_frg, sizeof(shd_log), NULL, shd_log);
-      fprintf(stderr, "[FRAG]\n%s\n", shd_log);
-   }
 
    u32 shd_id = glCreateProgram();
    glAttachShader(shd_id, shd_vrt);
@@ -72,15 +54,6 @@ Shader Graphics_CreateComputeShader(Graphics* graphics, const char* compute_shad
    u32 shd_cmp = glCreateShader(GL_COMPUTE_SHADER);
    glShaderSource(shd_cmp, 1, &compute_shader, NULL);
    glCompileShader(shd_cmp);
-
-   i32 cmp_sucess = 1;
-   glGetShaderiv(shd_cmp, GL_COMPILE_STATUS, &cmp_sucess);
-   if (!cmp_sucess)
-   {
-      char shd_log[1024] = { 0 };
-      glGetShaderInfoLog(shd_cmp, sizeof(shd_log), NULL, shd_log);
-      fprintf(stderr, "[COMP]\n%s\n", shd_log);
-   }
 
    u32 shd_id = glCreateProgram();
    glAttachShader(shd_id, shd_cmp);
@@ -118,6 +91,11 @@ u32 Graphics_GetUniformLocation(Graphics* graphics, Shader res_shader, const cha
       return UINT32_MAX;
 
    return glGetUniformLocation(shader.id.program, name);
+}
+
+void Graphics_SetUniform(Graphics* graphics, Uniform uniform)
+{
+   GFX_SetUniform(uniform);
 }
 
 void Graphics_Dispatch(Graphics* graphics, Shader res_shader, u32 size_x, u32 size_y, u32 size_z, UniformBlockList uniform_blocks)
