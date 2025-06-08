567f85600ff4262d70c17443d8574c9af5245312
Serenity Skiff <skiffserenity@gmail.com>
2025-06-08 08:55:23 -0500

    Misc. framebuffer updates

--- src/graphics/textures.c
+++ src/graphics/textures.c
@@ -29,7 +29,7 @@ Texture Graphics_CreateTexture(Graphics* graphics, u8* data, resolution2d size, 
 
    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
    glTexImage2D(GL_TEXTURE_2D, 0, format[channels], size.width, size.height, 0, format[channels], GL_UNSIGNED_BYTE, data);
@@ -71,16 +71,24 @@ Framebuffer Graphics_CreateFramebuffer(Graphics* graphics, resolution2d size)
    framebuffer.compare.ref = graphics->ref;
 
    glGenFramebuffers(1, &framebuffer.id.fbo);
+   
+   glBindFramebuffer(GL_FRAMEBUFFER, framebuffer.id.fbo);
    glGenRenderbuffers(1, &framebuffer.id.rbo);
 
-   glBindFramebuffer(GL_FRAMEBUFFER, framebuffer.id.fbo);
-
    glBindRenderbuffer(GL_RENDERBUFFER, framebuffer.id.rbo);
-   glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, (i32)size.width, (i32)size.height);
+   glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, size.width, size.height);
 
    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, framebuffer.id.rbo);
 
+   if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+   {
+      return (handle){ 0 };
+   }
+
    framebuffer.compare.handle = Util_ArrayLength(graphics->textures);
+
+   glBindFramebuffer(GL_FRAMEBUFFER, 0);
+   glBindRenderbuffer(GL_RENDERBUFFER, 0);
 
    return Util_AddResource(&graphics->ref, REF(graphics->framebuffers), &framebuffer);
 }
@@ -102,11 +110,13 @@ void Graphics_BindFramebuffer(Graphics* graphics, Framebuffer res_framebuffer)
       return;
 
    glBindFramebuffer(GL_FRAMEBUFFER, framebuffer.id.fbo);
+   glBindRenderbuffer(GL_RENDERBUFFER, framebuffer.id.rbo);
 }
 
 void Graphics_UnbindFramebuffers(Graphics *graphics)
 {
    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+   glBindRenderbuffer(GL_RENDERBUFFER, 0);
 }
 
 void Graphics_AttachTexturesToFramebuffer(Graphics* graphics, Framebuffer res_framebuffer, u32 texture_count, Texture res_textures[])
@@ -115,7 +125,10 @@ void Graphics_AttachTexturesToFramebuffer(Graphics* graphics, Framebuffer res_fr
    if (framebuffer.compare.ref != res_framebuffer.ref)
       return;
    
+   glBindTexture(GL_TEXTURE_2D, 0);
+
    glBindFramebuffer(GL_FRAMEBUFFER, framebuffer.id.fbo);
+   glBindRenderbuffer(GL_RENDERBUFFER, framebuffer.id.rbo);
 
    for (u32 i=0; i<texture_count; i++)
    {
@@ -126,5 +139,9 @@ void Graphics_AttachTexturesToFramebuffer(Graphics* graphics, Framebuffer res_fr
       glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0 + i, GL_TEXTURE_2D, texture.id.tex, 0);
    }
 
+   if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+      return;
+
    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+   glBindRenderbuffer(GL_RENDERBUFFER, 0);
 }
